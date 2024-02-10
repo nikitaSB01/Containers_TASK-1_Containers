5e6bd52134d97b13b760dc73975b8b3f12bfc992
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-02-10 13:03:15 +0600

    сделал тесты

--- src/js/_tests_/set.test.js
+++ src/js/_tests_/set.test.js
@@ -2,10 +2,80 @@
 import Character from '../Character';
 import Team from '../set';
 // начать тесты
+const player = new Character('Niki');
+test('class Character', () => {
+  expect(player).toEqual({
+    name: 'Niki',
+    level: 1,
+    health: 100,
+  });
+});
+test('Неверное имя', () => {
+  expect(() => new Character('R', 'Undead')).toThrow();
+});
 
+const newTeam = new Team('KTM');
 test('Создание новой команды Team', () => {
-  const newTeam = new Team('KTM');
   const result = { name: 'KTM', members: new Set() };
   expect(newTeam).toEqual(result);
 });
 
+const team = new Team('Reya');
+const unit1 = new Character('warrior');
+team.add(unit1);
+test('Добавление нового игрока методо add', () => {
+  const result = {
+    name: 'Reya',
+    members: new Set([{
+      name: 'warrior',
+      level: 1,
+      health: 100,
+    }]),
+  };
+  expect(team).toEqual(result);
+});
+
+test('Ошибка на добавление нового игрока методом add', () => {
+  expect(() => team.add(unit1)).toThrow();
+});
+
+test('Добавление нескольких пресонажий', () => {
+  const unit2 = new Character('igor');
+  const unit3 = new Character('vany');
+  team.addAll(unit2, unit3);
+  const result = {
+    name: 'Reya',
+    members: new Set([{
+      name: 'warrior',
+      level: 1,
+      health: 100,
+    }, {
+      name: 'igor',
+      level: 1,
+      health: 100,
+    }, {
+      name: 'vany',
+      level: 1,
+      health: 100,
+    }]),
+  };
+  expect(team).toEqual(result);
+});
+
+test('Вывод инфы о персонажах в масиве', () => {
+  const res = [
+    {
+      name: 'warrior',
+      level: 1,
+      health: 100,
+    }, {
+      name: 'igor',
+      level: 1,
+      health: 100,
+    }, {
+      name: 'vany',
+      level: 1,
+      health: 100,
+    }];
+  expect(team.toArray()).toEqual(res);
+});
